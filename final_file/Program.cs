bd1564aaacd5e152336e836f37116fc875dcd208
Kate <kd32107@inbox.ru>
2023-03-05 21:10:26 +0500

    Создание второго массива

--- final_file/Program.cs
+++ final_file/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3.
+
+string [] array = {"smart" , "123" , "-2" , "clever"};
+string [] secondArray = {};
+int count = 0 ;
+
+void ArrayMagic(string[] array, string[] secondArray)
+{
+    for(int i = 0 ; i<array.Length ; i++)
+    {
+        if (array[i].Length <= 3 )
+        {
+            secondArray[count] = array[i];
+            
+            Console.Write (secondArray[count]);
+            count++;
+        }
+    }
+}
+ArrayMagic(array, secondArray);
