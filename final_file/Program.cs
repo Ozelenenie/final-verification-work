2eb46979cf4fa8bfda3ef01d8fc1cd67915ab601
Kate <kd32107@inbox.ru>
2023-03-05 21:12:06 +0500

    Добавили вывод нового массива

--- final_file/Program.cs
+++ final_file/Program.cs
@@ -18,4 +18,15 @@ void ArrayMagic(string[] array, string[] secondArray)
         }
     }
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
 ArrayMagic(array, secondArray);
+PrintArray(secondArray);
